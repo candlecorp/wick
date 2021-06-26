8aabd2f8fc7166c97509f4bdaa5bb0d162c27d0a
Jarrod Overson <jsoverson@gmail.com>
2021-06-25 22:47:02 -0400

    normalized component output, extracted native provider, extracted models

--- crates/sdk/vino-transport/src/message_transport.rs
+++ crates/sdk/vino-transport/src/message_transport.rs
@@ -6,7 +6,7 @@ use serde::{
 };
 use vino_component::{
   v0,
-  Output,
+  Packet,
 };
 
 use crate::{
@@ -14,14 +14,17 @@ use crate::{
   Result,
 };
 
+pub type PortMap = HashMap<String, MessageTransport>;
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum MessageTransport {
-  MessagePack(Vec<u8>),
-  MultiBytes(HashMap<String, Vec<u8>>),
+  Invalid,
   Exception(String),
   Error(String),
+  MessagePack(Vec<u8>),
+  MultiBytes(HashMap<String, Vec<u8>>),
+  OutputMap(PortMap),
   Test(String),
-  Invalid,
 }
 
 impl Default for MessageTransport {
@@ -31,10 +34,29 @@ impl Default for MessageTransport {
 }
 
 impl MessageTransport {
+  pub fn is_ok(&self) -> bool {
+    match self {
+      MessageTransport::MessagePack(_) => true,
+      MessageTransport::MultiBytes(_) => true,
+      MessageTransport::Test(_) => true,
+      MessageTransport::Exception(_) => false,
+      MessageTransport::Error(_) => false,
+      MessageTransport::Invalid => false,
+      MessageTransport::OutputMap(_) => true,
+    }
+  }
   pub fn into_bytes(self) -> Result<Vec<u8>> {
     match self {
       MessageTransport::MessagePack(v) => Ok(v),
       _ => Err(Error::PayloadConversionError("Invalid payload".to_string())),
+    }
+  }
+  pub fn into_output_map(self) -> Result<PortMap> {
+    match self {
+      MessageTransport::OutputMap(v) => Ok(v),
+      _ => Err(Error::PayloadConversionError(
+        "Invalid payload, not an output map".to_string(),
+      )),
     }
   }
   pub fn into_multibytes(self) -> Result<HashMap<String, Vec<u8>>> {
@@ -63,10 +85,10 @@ impl From<&[u8]> for MessageTransport {
   }
 }
 
-impl From<Output> for MessageTransport {
-  fn from(output: Output) -> MessageTransport {
+impl From<Packet> for MessageTransport {
+  fn from(output: Packet) -> MessageTransport {
     match output {
-      Output::V0(v) => match v {
+      Packet::V0(v) => match v {
         v0::Payload::Exception(v) => MessageTransport::Exception(v),
         v0::Payload::Error(v) => MessageTransport::Error(v),
         v0::Payload::Invalid => MessageTransport::Invalid,
