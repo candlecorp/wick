7b3ee2b4c21ff305ec5656508b34a83e81e1eba3
Jarrod Overson <jsoverson@gmail.com>
2021-10-05 09:32:39 -0400

    refactor: reworked run_test into TestSuite

--- crates/vino-test/src/lib.rs
+++ crates/vino-test/src/lib.rs
@@ -18,6 +18,7 @@ use vino_transport::{
   MessageTransport,
   Success,
   TransportMap,
+  TransportWrapper,
 };
 
 use self::error::TestError;
@@ -25,12 +26,89 @@ use self::error::TestError;
 pub mod error;
 pub use error::TestError as Error;
 
+pub struct TestSuite {
+  tests: Vec<TestData>,
+  name: String,
+  filters: Vec<String>,
+}
+
+impl Default for TestSuite {
+  fn default() -> Self {
+    Self::new("Test")
+  }
+}
+
+impl TestSuite {
+  pub fn new<T: AsRef<str>>(name: T) -> Self {
+    Self {
+      tests: Vec::new(),
+      name: name.as_ref().to_owned(),
+      filters: Vec::new(),
+    }
+  }
+
+  pub fn try_from_file(file: PathBuf) -> Result<Self, TestError> {
+    let contents = read_to_string(file).map_err(|e| TestError::ReadFailed(e.to_string()))?;
+    let data: Vec<TestData> =
+      serde_yaml::from_str(&contents).map_err(|e| TestError::ParseFailed(e.to_string()))?;
+
+    Ok(TestSuite::from_tests(data))
+  }
+
+  pub fn from_tests(tests: Vec<TestData>) -> Self {
+    Self {
+      tests,
+      ..Default::default()
+    }
+  }
+
+  pub fn get_tests(&mut self) -> Vec<&mut TestData> {
+    let filters = &self.filters;
+    if !filters.is_empty() {
+      self
+        .tests
+        .iter_mut()
+        .filter(|test| {
+          filters
+            .iter()
+            .any(|filter| test.get_description().contains(filter))
+        })
+        .collect()
+    } else {
+      self.tests.iter_mut().collect()
+    }
+  }
+
+  pub fn filter(mut self, filters: Vec<String>) -> Self {
+    self.filters = filters;
+    self
+  }
+
+  pub fn name(mut self, name: String) -> Self {
+    self.name = name;
+    self
+  }
+
+  pub async fn run(&mut self, provider: BoxedRpcHandler) -> Result<TestRunner, TestError> {
+    let name = self.name.clone();
+    let tests = self.get_tests();
+    run_test(name, tests, provider).await
+  }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TestData {
   pub component: String,
-  pub description: Option<String>,
+  #[serde(default)]
+  pub description: String,
+  #[serde(default)]
+  pub init_data: HashMap<String, String>,
+  #[serde(default)]
   pub inputs: HashMap<String, serde_value::Value>,
+  #[serde(default)]
   pub outputs: Vec<OutputData>,
+  #[serde(skip)]
+  pub actual: Vec<TransportWrapper>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -46,55 +124,91 @@ pub struct SerializedTransport {
   pub error_msg: Option<String>,
 }
 
-pub fn read_data(path: PathBuf) -> Result<Vec<TestData>, error::TestError> {
-  let contents = read_to_string(path).map_err(|e| TestError::ReadFailed(e.to_string()))?;
-  let data: Vec<TestData> =
-    serde_yaml::from_str(&contents).map_err(|e| TestError::ParseFailed(e.to_string()))?;
-  Ok(data)
+impl TestData {
+  pub fn get_payload(&self) -> TransportMap {
+    let mut payload = TransportMap::new();
+    for (k, v) in &self.inputs {
+      payload.insert(k, MessageTransport::Success(Success::Serialized(v.clone())));
+    }
+
+    if !self.init_data.is_empty() {
+      payload.with_config(self.init_data.clone());
+    }
+    payload
+  }
+
+  pub fn get_description(&self) -> String {
+    let separator = if self.description.is_empty() {
+      ""
+    } else {
+      " : "
+    };
+    format!("{}{}{}", self.component, separator, self.description)
+  }
 }
 
 pub async fn run_test(
   name: String,
-  expected: Vec<TestData>,
+  expected: Vec<&mut TestData>,
   provider: BoxedRpcHandler,
 ) -> Result<TestRunner, Error> {
   let mut harness = TestRunner::new(Some(name));
 
   for test in expected {
-    let mut payload = TransportMap::new();
+    let payload = test.get_payload();
     let entity = Entity::component_direct(test.component.clone());
-    for (k, v) in &test.inputs {
-      payload.insert(k, MessageTransport::Success(Success::Serialized(v.clone())));
-    }
 
-    let stream = provider
+    let mut test_block = TestBlock::new(Some(test.get_description()));
+
+    let result = provider
       .invoke(entity, payload)
       .await
-      .map_err(|e| Error::InvocationFailed(e.to_string()))?;
+      .map_err(|e| Error::InvocationFailed(e.to_string()));
+
+    if let Err(e) = result {
+      test_block.add_test(
+        || false,
+        "Invocation",
+        Some(vec![format!("Invocation failed: {}", e)]),
+      );
+      harness.add_block(test_block);
+      continue;
+    }
+
+    let stream = result.unwrap();
 
     let outputs: Vec<_> = stream.collect().await;
-    let description = test
-      .description
-      .map_or_else(String::new, |desc| format!(" - {}", desc));
-    let mut test_block = TestBlock::new(Some(format!(
-      "Component '{}'{}",
-      test.component, description
-    )));
+    test.actual = outputs;
+    let mut diagnostics = vec!["Output: ".to_owned()];
+    let mut output_lines: Vec<_> = test.actual.iter().map(|o| format!("{:?}", o)).collect();
+    diagnostics.append(&mut output_lines);
+    test_block.diagnostics = diagnostics;
 
-    for (i, output) in test.outputs.into_iter().enumerate() {
-      let result = outputs[i].port == output.port;
+    for (i, expected) in test.outputs.iter().cloned().enumerate() {
+      if i >= test.actual.len() {
+        test_block.add_test(
+          || false,
+          "Stream length",
+          Some(vec![
+            "Test data included more output than component produced".to_owned(),
+          ]),
+        );
+        break;
+      }
+      let actual = &test.actual[i];
+      let result = actual.port == expected.port;
       let diag = Some(vec![
-        format!("Actual: {}", outputs[i].port),
-        format!("Expected: {}", output.port),
+        format!("Actual: {}", actual.port),
+        format!("Expected: {}", expected.port),
       ]);
       test_block.add_test(
         move || result,
-        format!("Output port name is '{}'", output.port),
+        format!("Output port name == '{}'", expected.port),
         diag,
       );
 
-      if let Some(value) = &output.payload.value {
-        let actual_payload = outputs[i].payload.clone();
+      if let Some(value) = &expected.payload.value {
+        let actual_payload = actual.payload.clone();
 
         let actual_value: Result<serde_value::Value, Error> = actual_payload
           .try_into()
@@ -114,15 +228,15 @@ pub async fn run_test(
 
         test_block.add_test(
           move || match actual_value {
-            Ok(val) => val == expected_value,
+            Ok(val) => eq(val, expected_value),
             Err(_e) => false,
           },
-          "Payload value matches",
+          "Payload value",
           diagnostic,
         );
       }
-      if let Some(error_kind) = output.payload.error_kind {
-        let actual_payload = outputs[i].payload.clone();
+      if let Some(error_kind) = expected.payload.error_kind {
+        let actual_payload = actual.payload.clone();
 
         let diag = Some(vec![format!(
           "Expected an {} error kind, but payload was: {:?}",
@@ -135,12 +249,12 @@ pub async fn run_test(
             MessageTransport::Failure(Failure::Error(_)) => (error_kind == "Error"),
             _ => false,
           },
-          "Error kind matches",
+          "Error kind",
           diag,
         );
       }
-      if let Some(error_msg) = output.payload.error_msg {
-        let actual_payload = outputs[i].payload.clone();
+      if let Some(error_msg) = expected.payload.error_msg {
+        let actual_payload = actual.payload.clone();
 
         let diag = Some(vec![format!(
           "Expected error message '{}', but payload was: {:?}",
@@ -153,12 +267,32 @@ pub async fn run_test(
             MessageTransport::Failure(Failure::Error(msg)) => (error_msg == msg),
             _ => false,
           },
-          "Error message matches",
+          "Error message",
           diag,
         );
       }
     }
     harness.add_block(test_block);
   }
+  harness.run();
   Ok(harness)
+}
+
+fn eq(left: serde_value::Value, right: serde_value::Value) -> bool {
+  promote_val(left) == promote_val(right)
+}
+
+fn promote_val(val: serde_value::Value) -> serde_value::Value {
+  use serde_value::Value;
+  match val {
+    Value::U8(n) => Value::U64(n.into()),
+    Value::U16(n) => Value::U64(n.into()),
+    Value::U32(n) => Value::U64(n.into()),
+    Value::I8(n) => Value::I64(n.into()),
+    Value::I16(n) => Value::I64(n.into()),
+    Value::I32(n) => Value::I64(n.into()),
+    Value::F32(n) => Value::F64(n.into()),
+    Value::Char(n) => Value::String(n.into()),
+    x => x,
+  }
 }
