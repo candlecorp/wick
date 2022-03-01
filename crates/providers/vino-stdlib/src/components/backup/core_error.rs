4276df502439c3ea1a8561166c41e892cfdf68fc
Jarrod Overson <jsoverson@gmail.com>
2022-03-01 13:31:33 -0500

    removed old components

--- crates/providers/vino-stdlib/src/components/backup/core_error.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::components::generated::core_error::*;
-
-pub(crate) async fn job(_input: Inputs, _output: OutputPorts, _context: crate::Context) -> JobResult {
-  Err(NativeComponentError::new("This component will always error"))
-}
--- crates/providers/vino-stdlib/src/components/backup/core_log.rs
+++
@@ -1,7 +0,0 @@
-pub use crate::components::generated::core_log::*;
-
-pub(crate) async fn job(input: Inputs, output: OutputPorts, _context: crate::Context) -> JobResult {
-  println!("Logger: {}", input.input);
-  output.output.done(Payload::success(&input.input))?;
-  Ok(())
-}
