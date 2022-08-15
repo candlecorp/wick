4e6e6d197238015bbbe937af9c0af0b784f49bed
Jarrod Overson <jsoverson@gmail.com>
2022-08-15 14:56:43 -0400

    added wafl tests with trycmd, added ability for wafl query to accept from STDIN (#76)

--- crates/bins/wafl/src/commands/query.rs
+++ crates/bins/wafl/src/commands/query.rs
@@ -1,9 +1,11 @@
+use std::io::{self, BufRead};
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use anyhow::Result;
 use clap::Args;
 use jaq_core::{parse, Ctx, Definitions, Val};
-use markup_converter::Transcoder;
+use markup_converter::{Format, Transcoder};
 
 #[derive(Debug, Clone, Args)]
 #[clap(rename_all = "kebab-case")]
@@ -15,18 +17,66 @@ pub(crate) struct Options {
   #[clap(short = 'r', long = "raw", action)]
   raw_output: bool,
 
+  /// Option to print raw output.
+  #[clap(short = 't', long = "type", action)]
+  kind: Option<MarkupKind>,
+
   /// Path to JSON, YAML, or TOML file.
-  #[clap(action)]
-  path: PathBuf,
+  #[clap(short = 'f', long = "file", action)]
+  path: Option<PathBuf>,
 
   /// The query.
   #[clap(action)]
   query: String,
 }
 
+#[derive(Debug, Clone)]
+enum MarkupKind {
+  Json,
+  Toml,
+  Yaml,
+}
+
+impl FromStr for MarkupKind {
+  type Err = anyhow::Error;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "json" => Ok(MarkupKind::Json),
+      "toml" => Ok(MarkupKind::Toml),
+      "yaml" => Ok(MarkupKind::Yaml),
+      _ => Err(anyhow!("Markup kind '{}' not supported", s)),
+    }
+  }
+}
+
 #[allow(clippy::unused_async)]
 pub(crate) async fn handle(opts: Options) -> Result<()> {
-  let input = Transcoder::new(&opts.path)?.to_json()?;
+  let input = if let Some(path) = opts.path {
+    match opts.kind {
+      None => Transcoder::from_path(&path)?.to_json()?,
+      Some(kind) => {
+        let source = crate::io::read_to_string(&path).await?;
+        match kind {
+          MarkupKind::Json => Transcoder::new(Format::json(&source)?)?.to_json()?,
+          MarkupKind::Toml => Transcoder::new(Format::toml(&source)?)?.to_json()?,
+          MarkupKind::Yaml => Transcoder::new(Format::yaml(&source)?)?.to_json()?,
+        }
+      }
+    }
+  } else {
+    if atty::is(atty::Stream::Stdin) {
+      eprintln!("No file path passed, reading from <STDIN>. Use -f/--file to pass a file as an argument.");
+    }
+    let reader = io::BufReader::new(io::stdin());
+    let lines = reader.lines();
+    let markup = lines.collect::<Result<String, _>>()?;
+    match opts.kind {
+      Some(MarkupKind::Json) | None => Transcoder::new(Format::json(&markup)?)?.to_json()?,
+      Some(MarkupKind::Toml) => Transcoder::new(Format::toml(&markup)?)?.to_json()?,
+      Some(MarkupKind::Yaml) => Transcoder::new(Format::yaml(&markup)?)?.to_json()?,
+    }
+  };
 
   let filter = &opts.query;
 
