ae5ffeaae6cb28d384d327c48cd743336f734b50
Phil Kedy <phil.kedy@gmail.com>
2023-01-09 17:40:59 -0500

    Reverting baseUri change

--- docsite/docusaurus.config.js
+++ docsite/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'NanoBus',
   tagline: 'Build production-level software with PoC-level of effort',
   url: 'https://nanobus.io',
-  baseUrl: '/docsite/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
