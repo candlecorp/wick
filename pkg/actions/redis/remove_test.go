13ba2d7ad2b253142976c01aa6a88941b41af630
Shazaib Nasrullah <>
2023-02-14 16:06:08 -0500

    Slight changes based on Phils comments

--- pkg/actions/redis/remove_test.go
+++ pkg/actions/redis/remove_test.go
@@ -26,63 +26,63 @@ import (
 func TestRemove(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
-	} else {
-		ctx := context.Background()
-
-		client := redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "",
-			DB:       0,
-		})
-
-		resources := resource.Resources{
-			"test": client,
-		}
-
-		jsonCodec := json_codec.NewCodec()
-		codecs := codec.Codecs{
-			"json": jsonCodec,
-		}
-
-		resolver := func(name string, target interface{}) bool {
-			switch name {
-			case "resource:lookup":
-				return resolve.As(resources, target)
-			case "codec:lookup":
-				return resolve.As(codecs, target)
-			}
-			return false
-		}
-
-		var res int64 = 0
-
-		expected := map[string]any{
-			"result": res,
-		}
-
-		data := actions.Data{
-			"input": map[string]any{
-				"key": "foo",
-			},
-		}
-
-		c, err := nanoredis.RemoveLoader(ctx, map[string]any{
-			"resource": "test",
-			"key":      "input.key",
-			"codec":    "json",
-		}, resolver)
-		require.NoError(t, err)
-
-		_, err = c(ctx, data)
-		require.NoError(t, err)
-
-		actual, err := c(ctx, data)
-		require.NoError(t, err)
-
-		map_result := map[string]interface{}{
-			"result": actual,
-		}
-
-		assert.Equal(t, expected, map_result)
+		return
 	}
+	ctx := context.Background()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	resources := resource.Resources{
+		"test": client,
+	}
+
+	jsonCodec := json_codec.NewCodec()
+	codecs := codec.Codecs{
+		"json": jsonCodec,
+	}
+
+	resolver := func(name string, target interface{}) bool {
+		switch name {
+		case "resource:lookup":
+			return resolve.As(resources, target)
+		case "codec:lookup":
+			return resolve.As(codecs, target)
+		}
+		return false
+	}
+
+	var res int64 = 0
+
+	expected := map[string]any{
+		"result": res,
+	}
+
+	data := actions.Data{
+		"input": map[string]any{
+			"key": "foo",
+		},
+	}
+
+	c, err := nanoredis.RemoveLoader(ctx, map[string]any{
+		"resource": "test",
+		"key":      "input.key",
+		"codec":    "json",
+	}, resolver)
+	require.NoError(t, err)
+
+	_, err = c(ctx, data)
+	require.NoError(t, err)
+
+	actual, err := c(ctx, data)
+	require.NoError(t, err)
+
+	map_result := map[string]interface{}{
+		"result": actual,
+	}
+
+	assert.Equal(t, expected, map_result)
 }
--- pkg/actions/redis/setget_test.go
+++ pkg/actions/redis/setget_test.go
@@ -26,73 +26,72 @@ import (
 func TestGet(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
-	} else {
-		ctx := context.Background()
+		return
+	}
+	ctx := context.Background()
 
-		client := redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "",
-			DB:       0,
-		})
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
 
-		resources := resource.Resources{
-			"test": client,
-		}
-
-		jsonCodec := json_codec.NewCodec()
-		codecs := codec.Codecs{
-			"json": jsonCodec,
-		}
-
-		resolver := func(name string, target interface{}) bool {
-			switch name {
-			case "resource:lookup":
-				return resolve.As(resources, target)
-			case "codec:lookup":
-				return resolve.As(codecs, target)
-			}
-			return false
-		}
-
-		expected := map[string]any{
-			"value": "bar",
-		}
-
-		inData := actions.Data{
-			"input": map[string]any{
-				"key":   "foo",
-				"value": "{\"value\":\"bar\"}",
-			},
-		}
-
-		b, err := nanoredis.SetLoader(ctx, map[string]any{
-			"resource": "test",
-			"key":      "input.key",
-			"data":     "input.value",
-			"codec":    "json",
-		}, resolver)
-		require.NoError(t, err)
-
-		_, err = b(ctx, inData)
-		require.NoError(t, err)
-
-		data := actions.Data{
-			"input": map[string]any{
-				"key": "foo",
-			},
-		}
-
-		a, err := nanoredis.GetLoader(ctx, map[string]any{
-			"resource": "test",
-			"key":      "input.key",
-			"codec":    "json",
-		}, resolver)
-		require.NoError(t, err)
-
-		actual, err := a(ctx, data)
-		require.NoError(t, err)
-
-		assert.Equal(t, expected, actual)
+	resources := resource.Resources{
+		"test": client,
 	}
 
+	jsonCodec := json_codec.NewCodec()
+	codecs := codec.Codecs{
+		"json": jsonCodec,
+	}
+
+	resolver := func(name string, target interface{}) bool {
+		switch name {
+		case "resource:lookup":
+			return resolve.As(resources, target)
+		case "codec:lookup":
+			return resolve.As(codecs, target)
+		}
+		return false
+	}
+
+	expected := map[string]any{
+		"value": "bar",
+	}
+
+	inData := actions.Data{
+		"input": map[string]any{
+			"key":   "foo",
+			"value": "{\"value\":\"bar\"}",
+		},
+	}
+
+	b, err := nanoredis.SetLoader(ctx, map[string]any{
+		"resource": "test",
+		"key":      "input.key",
+		"data":     "input.value",
+		"codec":    "json",
+	}, resolver)
+	require.NoError(t, err)
+
+	_, err = b(ctx, inData)
+	require.NoError(t, err)
+
+	data := actions.Data{
+		"input": map[string]any{
+			"key": "foo",
+		},
+	}
+
+	a, err := nanoredis.GetLoader(ctx, map[string]any{
+		"resource": "test",
+		"key":      "input.key",
+		"codec":    "json",
+	}, resolver)
+	require.NoError(t, err)
+
+	actual, err := a(ctx, data)
+	require.NoError(t, err)
+
+	assert.Equal(t, expected, actual)
 }
